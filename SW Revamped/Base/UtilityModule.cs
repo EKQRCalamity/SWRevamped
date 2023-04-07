a4fa2afbce12c9eaa14d980b97cb33d3819d99c2
EKQRCalamity <EKQRCalamity@KOTLIN>
2023-04-07 10:20:29 +0200

    Added WardManager and UtilityModule Base class

---
+++ SW Revamped/Base/UtilityModule.cs
@@ -0,0 +1,22 @@
+﻿using SWRevamped.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWRevamped.Base
+{
+    internal abstract class UtilityModule
+    {
+        public abstract string Name { get; }
+        public abstract string Version { get; }
+        public abstract string Description { get; }
+        public abstract string Author { get; }
+
+        internal WardManager WardManager { get; } = new WardManager();
+
+        internal abstract void Init();
+
+    }
+}
---
+++ SW Revamped/Utility/WardManager.cs
@@ -0,0 +1,57 @@
+﻿using Oasys.Common.Extensions;
+using Oasys.Common.GameObject;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWRevamped.Utility
+{
+    internal class Ward
+    {
+        internal string Name { get; }
+
+        internal Vector3 MovePosition { get; }
+        internal Vector3 ClickPosition { get; }
+        internal Vector3 WardPosition { get; }
+
+        internal Ward(string name, Vector3 movePosition, Vector3 clickPosition, Vector3 wardPosition)
+        {
+            Name = name;
+            MovePosition = movePosition;
+            ClickPosition = clickPosition;
+            WardPosition = wardPosition;
+        }
+    }
+    internal class WardManager
+    {
+        private List<Ward> KnownWards  = new List<Ward>();
+
+        private void ConstructWards()
+        {
+            KnownWards = new List<Ward>()
+            {
+
+
+            };
+        }
+
+        internal WardManager() 
+        {
+            ConstructWards();    
+        }
+        
+        internal List<Ward> GetKnownWards()
+        {
+            return KnownWards;
+        }
+
+        internal Ward? GetClosestWard(GameObjectBase target)
+        {
+            return GetKnownWards().OrderBy(x => x.MovePosition.DistanceToPlayer()).FirstOrDefault();
+        }
+
+    }
+}
